10268764e09269119d48587cd0e25f44dc860fd0
Sarah Fowler <sarah.fowler73@gmail.com>
2019-02-27 14:45:16 -0500

    Updates

---
+++ precache-manifest.912a78a03308d11d526d6318a7b5a00c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6c821a62c7e74884ac6",
+    "url": "/Boggle/static/css/main.490bb118.chunk.css"
+  },
+  {
+    "revision": "c6c821a62c7e74884ac6",
+    "url": "/Boggle/static/js/main.c6c821a6.chunk.js"
+  },
+  {
+    "revision": "e143e6b1f187fec44772",
+    "url": "/Boggle/static/js/runtime~main.e143e6b1.js"
+  },
+  {
+    "revision": "603fa182a2a582688b88",
+    "url": "/Boggle/static/js/2.603fa182.chunk.js"
+  },
+  {
+    "revision": "fe5ddcf5051cc1a228082fd1661e9a61",
+    "url": "/Boggle/index.html"
+  }
+];
