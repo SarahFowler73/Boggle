bb99a3efa737dfcb953429f56db6b91b98410661
Sarah Fowler <sarah.fowler73@gmail.com>
2019-03-01 15:06:32 -0500

    Updates

---
+++ precache-manifest.20d3a195023256449b586cae4339aba6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4f6a06e657240dec78f",
+    "url": "/Boggle-Solver/static/css/main.c1684db8.chunk.css"
+  },
+  {
+    "revision": "d4f6a06e657240dec78f",
+    "url": "/Boggle-Solver/static/js/main.d4f6a06e.chunk.js"
+  },
+  {
+    "revision": "3fad5883e2f567f240ae",
+    "url": "/Boggle-Solver/static/js/runtime~main.3fad5883.js"
+  },
+  {
+    "revision": "188dffc5b4e715f949fe",
+    "url": "/Boggle-Solver/static/js/2.188dffc5.chunk.js"
+  },
+  {
+    "revision": "41037a39c51f2582d353403074235047",
+    "url": "/Boggle-Solver/index.html"
+  }
+];
